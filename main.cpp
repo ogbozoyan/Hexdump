67701a75f104f6ef9a5fce9ca72bf6f50d74102a
ogbozoyan <85520525+ogbozoyan@users.noreply.github.com>
2022-10-26 00:40:49 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,21 +9,28 @@ char bytes[16];
 FILE* f;
 
 
-void printar(){
-    printf("%X.%X.%X.%X %X.%X.%X.%X %X.%X.%X.%X %X.%X.%X.%X || %c%c%c%c   %c%c%c%c  %c%c%c%c  %c%c%c%c \n",
+void printar(int i){
+    mvprintw(i%150,0,"%X%X%X%X %X%X%X%X %X%X%X%X %X%X%X%X  || \t  %c%c%c%c   %c%c%c%c  %c%c%c%c  %c%c%c%c \n",
            bytes[0],bytes[1],bytes[2],bytes[3],bytes[4],bytes[5],bytes[6],bytes[7],bytes[8],bytes[9],bytes[10],bytes[11],bytes[12],bytes[13],bytes[14],bytes[15],
            bytes[0],bytes[1],bytes[2],bytes[3],bytes[4],bytes[5],bytes[6],bytes[7],bytes[8],bytes[9],bytes[10],bytes[11],bytes[12],bytes[13],bytes[14],bytes[15]);
+
 }
-void cclear(){
-    for(int i = 0;i<16;i++){
-        if(bytes[i] == '\n'){
-            bytes[i] = NULL;
-        }
+
+int init_ncurses(){
+    if( NULL == initscr()) {
+        return __LINE__;
     }
+    noecho();
+    curs_set(0);
+    keypad(stdscr,TRUE);
+    return 1;
 }
+
 int main(int argc,char **argv) {
+
     char *namefile = argv[1];
-    char ch = 0;
+    char ch,key = 0;
+    bool flag = true;
     int size = 0;
     if (argc < 2) {
         printf("Usage: %s name_of_file", argv[0]);
@@ -34,22 +41,34 @@ int main(int argc,char **argv) {
         fprintf(stderr, "Couldn't open %s: %s\n", argv[1], strerror(errno));
         exit(1);
     }
-    fseek(f, 0, SEEK_SET);
-    while(1)
-    {
+
+    while (1) {
         ch = fgetc(f);
-        if(feof(f)){
+        if (feof(f)) {
             break;
         }
         size += 1;
     }
-    fseek(f, 0, SEEK_SET);
-    for(int j = 0;j<size/16+(size%16);j++){
-        fread(&bytes, 1, 16, f);
-        cclear();
-        printar();
-    }
 
+    fseek(f, 0, SEEK_SET);
+    init_ncurses();
+    do {
+        switch (key = getch()) {
+            case 'p':
+                for (int j = 0; j < 50; j++) {
+                    fread(&bytes, 1, 16, f);
+                    printar(j);
+                    refresh();
+                }
+                break;
+            case 'q':
+                flag = false;
+                break;
+            default:
+                break;
+            }
+    }while(flag);
+    endwin();
     fclose(f);
     return 0;
 }
