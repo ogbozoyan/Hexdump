4920620977fedb74af96383391c3b726f5fc8335
ogbozoyan <85520525+ogbozoyan@users.noreply.github.com>
2022-10-26 19:07:45 +0300

    alpha

--- main.cpp
+++ main.cpp
@@ -3,17 +3,16 @@
 #include <vector>
 #include <ncurses.h>
 
-
 using namespace std;
 
 FILE* f;
 char bytes[16];
 char clearbytes[16];
-int OFFSET = 1;
+int OFFSET = 0;
 char ch,key = 0;
 bool flag = true;
+bool eof = true;
 int line = 1;
-
 
 void clear_bytes(){
     for(int i =0;i<sizeof(bytes);i++){
@@ -21,6 +20,7 @@ void clear_bytes(){
         else clearbytes[i] = bytes[i];
     }
 }
+
 int init_ncurses(){
     if( NULL == initscr()) {
         return __LINE__;
@@ -30,20 +30,41 @@ int init_ncurses(){
     keypad(stdscr,TRUE);
     return 1;
 }
-void print_file(){
+
+void print_file() {
     fread(&bytes, 1, 16, f);
     clear_bytes();
-    if(line%49 == 0){
+    if (line % 49 == 0) {
         clear();
     }
-    mvprintw(line%49,0,"%x%x%x%x %x%x%x%x %x%x%x%x %x%x%x%x || %c%c%c%c   %c%c%c%c  %c%c%c%c  %c%c%c%c \n",
-             bytes[0],bytes[1],bytes[2],bytes[3],bytes[4],bytes[5],bytes[6],bytes[7],bytes[8],bytes[9],bytes[10],bytes[11],bytes[12],bytes[13],bytes[14],bytes[15],
-             clearbytes[0],clearbytes[1],clearbytes[2],clearbytes[3],clearbytes[4],clearbytes[5],clearbytes[6],clearbytes[7],clearbytes[8],clearbytes[9],clearbytes[10],
-             clearbytes[11],clearbytes[12],clearbytes[13],clearbytes[14],clearbytes[15]);
-    refresh();
-    line++;
-    OFFSET++;
+    if (!feof(f)){
+        mvprintw(line % 49, 0, "%#010x || %x%x%x%x %x%x%x%x %x%x%x%x %x%x%x%x || %c%c%c%c   %c%c%c%c  %c%c%c%c  %c%c%c%c \n",
+                 OFFSET, bytes[0], bytes[1], bytes[2], bytes[3], bytes[4], bytes[5], bytes[6], bytes[7], bytes[8],
+                 bytes[9], bytes[10], bytes[11], bytes[12], bytes[13], bytes[14], bytes[15],
+                 clearbytes[0], clearbytes[1], clearbytes[2], clearbytes[3], clearbytes[4], clearbytes[5],
+                 clearbytes[6], clearbytes[7], clearbytes[8], clearbytes[9], clearbytes[10],
+                 clearbytes[11], clearbytes[12], clearbytes[13], clearbytes[14], clearbytes[15]);
+        //eof = false;
+        refresh();
+        line++;
+        OFFSET+=16;
+    }
+    else{
+        fread(&bytes, 1, 16, f);
+        clear_bytes();
+        mvprintw(line % 49, 0, "%#010x || %x%x%x%x %x%x%x%x %x%x%x%x %x%x%x%x || %c%c%c%c   %c%c%c%c  %c%c%c%c  %c%c%c%c \n",
+                 OFFSET, bytes[0], bytes[1], bytes[2], bytes[3], bytes[4], bytes[5], bytes[6], bytes[7], bytes[8],
+                 bytes[9], bytes[10], bytes[11], bytes[12], bytes[13], bytes[14], bytes[15],
+                 clearbytes[0], clearbytes[1], clearbytes[2], clearbytes[3], clearbytes[4], clearbytes[5],
+                 clearbytes[6], clearbytes[7], clearbytes[8], clearbytes[9], clearbytes[10],
+                 clearbytes[11], clearbytes[12], clearbytes[13], clearbytes[14], clearbytes[15]);
+        //eof = true;
+        refresh();
+        line++;
+        OFFSET+=16;
+    }
 }
+
 int main(int argc,char **argv) {
 
     char *namefile = argv[1];
@@ -52,13 +73,15 @@ int main(int argc,char **argv) {
         printf("Usage: %s name_of_file", argv[0]);
         return -1;
     }
+
     f = fopen(argv[1], "rb");
     if (f == NULL) {
         fprintf(stderr, "Couldn't open %s: %s\n", argv[1], strerror(errno));
         exit(1);
     }
+
     int size = 0;
-    while (1) {
+    while (true) {
         ch = fgetc(f);
         if (feof(f)) {
             break;
@@ -67,16 +90,17 @@ int main(int argc,char **argv) {
     }
 
     fseek(f, 0, SEEK_SET);
-
     init_ncurses();
+
     do {
         switch (key = getch()){
                 case 's':
-                    for(int i=0;i<48;i++) print_file();
+                    if(eof)
+                        for(int i=0;i<48;i++) print_file();
                 break;
                 case 'w':
-                    fseek(f,ftell(f)-96,SEEK_CUR);
-                    for(int i=0;i<48;i++) print_file();
+                    if(eof)
+                        for(int i=0;i<48;i++) print_file();
                 break;
                 case 'q':
                     flag = false;
@@ -86,6 +110,7 @@ int main(int argc,char **argv) {
                 break;
             }
     }while(flag);
+
     endwin();
     fclose(f);
     return 0;
